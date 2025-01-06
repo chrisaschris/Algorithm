bff450b32f88043e7730475907815f770cc0889b
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-01-06 18:00:26 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,7 +2,7 @@ def solution(array, commands):
     answer = []
     tmp = []
     for i in commands :
-        tmp=(array[i[0]-1:i[1]]).copy()
+        tmp=(array[i[0]-1:i[1]])
         tmp.sort()
         answer.append(tmp[i[2]-1])
     return answer
