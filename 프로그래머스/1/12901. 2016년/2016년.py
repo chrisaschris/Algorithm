e99e3e1c5411aada919483059c377f4ad62b4272
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-01-21 20:31:48 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,27 +1,4 @@
-def solution(a, b):
-    answer = ''
-    days = ['MON','TUE','WED','THU','FRI','SAT','SUN']
-    tmp=3
-    i=1
-    j=1
-    while 1:
-        answer = days[(j+tmp)%7]
-        if a==i and b==j:
-            break
-        # print(f'{i}month {j}days {answer}days')
-        if (i == 1 or i == 3 or i == 5 or i == 7 or i == 8 or i == 10 or i == 12) and j == 31:
-            i=i+1
-            tmp=(j+tmp)%7
-            j=1
-        elif (i==2 and j==29):
-            i=i+1
-            tmp=(j+tmp)%7
-            j=1
-        elif (i==2 or i==4 or i==6 or i==9 or i==11) and j==30:
-            i=i+1
-            tmp=(j+tmp)%7
-            j=1
-        else:
-            j=j+1
-    return answer
-
+def solution(a,b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return day[(sum(month[:a-1])+b-1)%7]
