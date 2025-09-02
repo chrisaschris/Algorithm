4d6701d229a5da440c9323979757a9e4fb56a1ed
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-09-02 10:36:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -13,3 +13,10 @@ def solution(n, words):
             answer[1]=i//n+1
             break
     return answer
+
+
+# def solution(n, words):
+#     for p in range(1, len(words)):
+#         if words[p][0] != words[p-1][-1] or words[p] in words[:p]: return [(p%n)+1, (p//n)+1]
+#     else:
+#         return [0,0]
