c0b16bcc689c411603f4ded94165ab4671f059c9
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-09-04 20:29:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    num = s.split(' ')
+    # print(num)
+    num_int = [int(x) for x in num]
+    num_int.sort()
+    # print(num_int)
+    answer = f"{num_int[0]} {num_int[-1]}"
+    return answer
