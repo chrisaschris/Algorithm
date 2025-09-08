59e8296d7043c71008041534262163685b0cf812
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-09-08 17:41:14 +0900

    [level 2] Title: 할인 행사, Time: 183.88 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,10 +3,8 @@ from collections import Counter
 def solution(want, number, discount):
     answer = 0
     want_counter = Counter(dict(zip(want, number)))
-    # print(want_counter)
     for i in range(0, len(discount)-9):
         wantdis_counter = Counter(discount[i:i+10])
-        # print(want_counter, wantdis_counter)
         if want_counter == wantdis_counter:
             answer = answer + 1
     return answer
