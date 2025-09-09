26de750a5d0b833f9034064f5458dd1e25a2622e
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-09-09 18:39:14 +0900

    [level 2] Title: 프로세스, Time: 0.74 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,23 +2,15 @@ def solution(priorities, location):
     answer = 0
     loc = [0]*len(priorities)
     loc[location]=1
-    # print(loc)
-    count=0
-    # print("first",priorities)
     while(len(priorities)):
         if priorities[0]==max(priorities):
             priorities.pop(0)
-            count+=1
+            answer+=1
             if(loc[0]==1):
                 break
             else:
                 loc.pop(0)
-            # print(priorities)
-            # print(loc)
         else:
             priorities.append(priorities.pop(0))
             loc.append(loc.pop(0))
-            # print("changed priorities",priorities)
-            # print("changed loc",loc)
-    answer = count
     return answer
