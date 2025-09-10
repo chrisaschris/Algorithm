81fddcd2d9349e59f320f3cbf5b4bd27fd410c59
chrisaschris <127742306+chrisaschris@users.noreply.github.com>
2025-09-10 15:51:25 +0900

    [level 2] Title: 전화번호 목록, Time: 492.71 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -3,7 +3,6 @@ def solution(phone_book):
     phone_book_set = set(phone_book)
     for num in phone_book:
         for i in range(1, len(num)):
-            # if num[:i+1].isin(phone_book_set):
             if num[:i] in (phone_book_set):
                 answer = False
                 break
